01a0867e84b552c7f80b03983c642c7ec62ec668
KyleMatthewC <kyle_matthew_chua@dlsu.edu.ph>
2016-11-13 18:38:51 +0800

    Minor Changes

--- project/WindowsApplication2/wdwDailyAttendanceLog.vb
+++ project/WindowsApplication2/wdwDailyAttendanceLog.vb
@@ -1,5 +1,4 @@
-﻿Imports MySql.Data.MySqlClient
-Public Class wdwDailyAttendanceLog
+﻿Public Class wdwDailyAttendanceLog
     Dim dbAccess As DatabaseAccessor = New DatabaseAccessor
 
     Private Sub Back_Click(sender As Object, e As EventArgs) Handles bttnBack.Click
@@ -28,14 +27,14 @@ Public Class wdwDailyAttendanceLog
     End Sub
 
     Private Sub wdwDailyAttendanceLog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Load_table()
+        Load_form()
     End Sub
 
     Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles dtp.ValueChanged
-        Load_table()
+        Load_form()
     End Sub
 
-    Private Sub Load_table()
+    Private Sub Load_form()
         dbAccess.fillDataGrid("Select a.attendanceid 'Reference No', a.absent_date 'Absent Date', f.facultyid 'Faculty ID', concat(f_lastname, ', ', f.f_firstname, ' ', f_middlename) 'Name', concat(cl.course_cd, ' ', c.section) 'Course/Section', concat(c.daysched, ' ', c.timestart, ' - ', c.timeend) 'Day/Time', c.room 'Room', r.remark_des 'Remarks', a.enc_date 'Date Encoded', a.encoder 'Encoder' from introse.attendance a, introse.faculty f, introse.courseoffering c, introse.course cl, introse.remarks r where a.courseoffering_id = c.courseoffering_id and c.course_id = cl.course_id and c.facref_no = f.facref_no and a.remarks_cd = r.remark_cd and a.status = 'A' and a.absent_date = '" & dtp.Value.Date & "' order by 4;", grid)
     End Sub
 
@@ -50,7 +49,7 @@ Public Class wdwDailyAttendanceLog
         If FacultyIDButton.Checked = True Then
 
             FirstName = dbAccess.getStringData("Select f_firstname from faculty where facultyid = '" & FacultyIDSearchText.Text & "';", "f_firstname")
-        MiddleName = dbAccess.getStringData("Select f_middlename from faculty where facultyid = '" & FacultyIDSearchText.Text & "';", "f_middlename")
+            MiddleName = dbAccess.getStringData("Select f_middlename from faculty where facultyid = '" & FacultyIDSearchText.Text & "';", "f_middlename")
             LastName = dbAccess.getStringData("Select f_lastname from faculty where facultyid = '" & FacultyIDSearchText.Text & "';", "f_lastname")
             FacultyID = dbAccess.getStringData("Select facultyid from faculty where facultyid = '" & FacultyIDSearchText.Text & "';", "facultyid")
 
--- project/WindowsApplication2/wdwFacultyMakeUp.Designer.vb
+++ project/WindowsApplication2/wdwFacultyMakeUp.Designer.vb
@@ -68,9 +68,9 @@ Partial Class wdwFacultyMakeUp
         'bttnBack
         '
         Me.bttnBack.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.bttnBack.Location = New System.Drawing.Point(507, 585)
+        Me.bttnBack.Location = New System.Drawing.Point(915, 630)
         Me.bttnBack.Name = "bttnBack"
-        Me.bttnBack.Size = New System.Drawing.Size(117, 23)
+        Me.bttnBack.Size = New System.Drawing.Size(175, 35)
         Me.bttnBack.TabIndex = 20
         Me.bttnBack.Text = "Back"
         Me.bttnBack.UseVisualStyleBackColor = True
@@ -78,9 +78,9 @@ Partial Class wdwFacultyMakeUp
         'bttnDelete
         '
         Me.bttnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.bttnDelete.Location = New System.Drawing.Point(370, 585)
+        Me.bttnDelete.Location = New System.Drawing.Point(625, 630)
         Me.bttnDelete.Name = "bttnDelete"
-        Me.bttnDelete.Size = New System.Drawing.Size(117, 23)
+        Me.bttnDelete.Size = New System.Drawing.Size(175, 35)
         Me.bttnDelete.TabIndex = 21
         Me.bttnDelete.Text = "Delete"
         Me.bttnDelete.UseVisualStyleBackColor = True
@@ -88,9 +88,9 @@ Partial Class wdwFacultyMakeUp
         'bttnModify
         '
         Me.bttnModify.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.bttnModify.Location = New System.Drawing.Point(224, 585)
+        Me.bttnModify.Location = New System.Drawing.Point(335, 630)
         Me.bttnModify.Name = "bttnModify"
-        Me.bttnModify.Size = New System.Drawing.Size(117, 23)
+        Me.bttnModify.Size = New System.Drawing.Size(175, 35)
         Me.bttnModify.TabIndex = 22
         Me.bttnModify.Text = "Modify"
         Me.bttnModify.UseVisualStyleBackColor = True
@@ -98,9 +98,9 @@ Partial Class wdwFacultyMakeUp
         'bttnAdd
         '
         Me.bttnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.bttnAdd.Location = New System.Drawing.Point(81, 585)
+        Me.bttnAdd.Location = New System.Drawing.Point(45, 630)
         Me.bttnAdd.Name = "bttnAdd"
-        Me.bttnAdd.Size = New System.Drawing.Size(117, 23)
+        Me.bttnAdd.Size = New System.Drawing.Size(175, 35)
         Me.bttnAdd.TabIndex = 23
         Me.bttnAdd.Text = "Add"
         Me.bttnAdd.UseVisualStyleBackColor = True
@@ -108,9 +108,9 @@ Partial Class wdwFacultyMakeUp
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(13, 252)
+        Me.DataGridView1.Location = New System.Drawing.Point(25, 265)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(685, 313)
+        Me.DataGridView1.Size = New System.Drawing.Size(1085, 350)
         Me.DataGridView1.TabIndex = 24
         '
         'GroupBox4
@@ -122,9 +122,9 @@ Partial Class wdwFacultyMakeUp
         Me.GroupBox4.Controls.Add(Me.Label1)
         Me.GroupBox4.Controls.Add(Me.Label6)
         Me.GroupBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox4.Location = New System.Drawing.Point(62, 138)
+        Me.GroupBox4.Location = New System.Drawing.Point(290, 150)
         Me.GroupBox4.Name = "GroupBox4"
-        Me.GroupBox4.Size = New System.Drawing.Size(558, 106)
+        Me.GroupBox4.Size = New System.Drawing.Size(560, 110)
         Me.GroupBox4.TabIndex = 176
         Me.GroupBox4.TabStop = False
         Me.GroupBox4.Text = "Faculty Information:"
@@ -310,7 +310,7 @@ Partial Class wdwFacultyMakeUp
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(710, 613)
+        Me.ClientSize = New System.Drawing.Size(1134, 671)
         Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.dtp)
