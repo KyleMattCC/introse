e44bb75e324bc23e66a07c9c4d3a21aab6a90539
jasonsy1 <jason_sy@dlsu.edu.ph>
2016-12-17 21:58:50 +0800

    updated db

--- project/introse.sql
+++ project/introse.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: introse
 -- ------------------------------------------------------
--- Server version	5.7.16-log
+-- Server version	5.7.14-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -57,7 +57,7 @@ CREATE TABLE `account` (
   `username` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (101,'Jason Sy','syjason','Lead','Jason'),(102,'Ralph Bravante','introse','Regular','Ralph'),(103,'Ged','gedagustin','Regular','Regular'),(104,'Kyle Chuapapi','chuakyle','Lead','Lead');
+INSERT INTO `account` VALUES (101,'Jason Sy','syjason','Lead','Jason'),(102,'Ralph Bravante','introse','Regular','Ralph'),(103,'Ged','gedagustin','Regular','Regular'),(104,'Kyle Chuapapi','chuakyle','Lead','Lead'),(105,'Sir Ryan','lasalle','Lead','RyanPogi');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +89,7 @@ CREATE TABLE `attendance` (
   `report_status` varchar(45) NOT NULL DEFAULT 'Pending',
   PRIMARY KEY (`attendanceid`),
   UNIQUE KEY `attendanceid_UNIQUE` (`attendanceid`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `attendance` (
 
 LOCK TABLES `attendance` WRITE;
 /*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
-INSERT INTO `attendance` VALUES (1,'2016-11-16',1,'LA','2016-11-21','Jason','Dixie','A','Pending'),(2,'2016-11-24',2,'ED','2016-12-05','Ged','Dixie','D','Pending'),(6,'2016-11-21',3,'ED','2016-11-21','Ged','Eric','A','Pending'),(16,'2016-12-15',5,'SW','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(17,'2016-12-15',9,'XX','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(18,'2016-12-15',10,'XX','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(19,'2016-12-15',12,'XX','2016-12-15','Kyle Chuapapi','Noel','A','Generated');
+INSERT INTO `attendance` VALUES (1,'2016-11-16',1,'LA','2016-11-21','Jason','Dixie','A','Pending'),(2,'2016-11-24',2,'ED','2016-12-05','Ged','Dixie','D','Pending'),(6,'2016-11-21',3,'ED','2016-11-21','Ged','Eric','A','Pending'),(16,'2016-12-15',5,'SW','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(17,'2016-12-15',9,'XX','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(18,'2016-12-15',10,'XX','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(19,'2016-12-15',12,'XX','2016-12-15','Kyle Chuapapi','Noel','A','Generated'),(20,'2016-12-16',13,'LA','2016-12-16','Jason Sy','Eric Dixie','A','Generated');
 /*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,12 +136,11 @@ DROP TABLE IF EXISTS `course`;
 CREATE TABLE `course` (
   `course_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `course_cd` char(7) NOT NULL,
-  `course_name` varchar(150) NOT NULL,
   `units` float unsigned NOT NULL,
   `offered_to` char(1) NOT NULL,
   PRIMARY KEY (`course_id`),
   UNIQUE KEY `course_id_UNIQUE` (`course_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,7 +149,7 @@ CREATE TABLE `course` (
 
 LOCK TABLES `course` WRITE;
 /*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES (1,'OBJECTP','Object Oriented Programming ',3,'U'),(2,'PERSEF1','Personal Effectiveness 1',2,'U'),(3,'COMPRO1','Computer Programming 1',3,'U'),(4,'COMPRO2','Computer Programming 2',3,'U'),(5,'LBY-IOT','Internet of Things - Laboratory',1,'U'),(6,'COMPASM','Computer Programming using Assembly',3,'U'),(7,'COMORGA','Computer Organization',3,'U'),(8,'PERSEF2','Personal Effectiveness 2',2,'U'),(9,'SOCTEC1','Science, Technology and Society 1',3,'U'),(10,'SOCTEC2','Science, Technology and Society 2',3,'U'),(11,'CMPILER','Compiler Theory',3,'U'),(12,'SECURDE','Secure Web Development',3,'U'),(13,'SWDESPA','Software Design Patterns',3,'U'),(14,'SOCTEC4','Science, Technology, and Society 4',3,'G'),(15,'ADVANDB','Advance Database',3,'G'),(16,'MASTERL','Master Lec',2,'G');
+INSERT INTO `course` VALUES (1,'OBJECTP',3,'U'),(2,'PERSEF1',2,'U'),(3,'COMPRO1',3,'U'),(4,'COMPRO2',3,'U'),(5,'LBY-IOT',1,'U'),(6,'COMPASM',3,'U'),(7,'COMORGA',3,'U'),(8,'PERSEF2',2,'U'),(9,'SOCTEC1',3,'U'),(10,'SOCTEC2',3,'U'),(11,'CMPILER',3,'U'),(12,'SECURDE',3,'U'),(13,'SWDESPA',3,'U'),(14,'SOCTEC4',3,'G'),(15,'ADVANDB',3,'G'),(16,'MASTERL',2,'G'),(17,'PERSEF9',3,'G'),(18,'PERSEF8',2,'U'),(19,'KASPIL1',3,'U');
 /*!40000 ALTER TABLE `course` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +174,7 @@ CREATE TABLE `courseoffering` (
   `status` char(1) NOT NULL,
   PRIMARY KEY (`courseoffering_id`),
   UNIQUE KEY `courseoffering_id_UNIQUE` (`courseoffering_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,7 +183,7 @@ CREATE TABLE `courseoffering` (
 
 LOCK TABLES `courseoffering` WRITE;
 /*!40000 ALTER TABLE `courseoffering` DISABLE KEYS */;
-INSERT INTO `courseoffering` VALUES (1,1,1,1,'S17','G202','MW',0915,1045,1.5,'A'),(2,1,1,2,'K42','V501','TH',0730,0900,1.5,'A'),(3,2,1,3,'EJ','A1107','MW',1430,1600,1.5,'A'),(4,1,1,2,'S15','G306A','MW',1100,1230,1.5,'A'),(5,3,1,2,'S12','G306A','TH',1100,1230,1.5,'A'),(6,4,1,2,'S13','G306A','MW',0915,1045,1.5,'A'),(7,13,1,2,'S14','G204','MW',1615,1745,1.5,'A'),(8,6,1,1,'S12','G201','MW',1100,1230,1.5,'A'),(9,7,1,1,'S13','G203','TH',1100,1230,1.5,'A'),(10,11,1,1,'S14','G204','TH',1430,1600,1.5,'A'),(11,5,1,1,'S15','G407','F',0915,1215,3,'A'),(12,10,1,3,'EE','J201','TH',0730,0900,1.5,'A'),(13,8,1,3,'C36','MM206','F',0915,1215,3,'A'),(14,10,1,3,'EF','J203','TH',1430,1600,1.5,'A'),(15,9,1,3,'EG','G201','MW',1615,1745,1.5,'A'),(16,14,1,4,'X21','LS110','MW',1245,1415,1.5,'A'),(17,15,1,4,'S11','Y304','TH',0915,1045,1.5,'A'),(18,16,1,5,'C31','V201','MW',0730,1030,3,'A'),(19,14,1,5,'EE','V202','MW',1100,1230,1.5,'A'),(20,14,1,5,'EC','V202','TH',1245,1415,1.5,'A');
+INSERT INTO `courseoffering` VALUES (26,17,1,6,'S14','G101','MW',1245,1415,1.5,'A'),(27,18,1,1,'EK','G101','MW',1245,1415,1.5,'A'),(28,3,1,7,'S17','G302B','TH',0915,1045,1.5,'A'),(29,1,1,2,'S18','G301A','TH',0730,0900,1.5,'A'),(30,19,1,3,'EC','G401','TH',1430,1600,1.5,'A'),(31,17,1,6,'S14','G101','MW',1245,1415,1.5,'A'),(32,18,1,1,'EK','G101','MW',1245,1415,1.5,'A'),(33,3,1,7,'S17','G302B','TH',0915,1045,1.5,'A'),(34,1,1,2,'S18','G301A','TH',0730,0900,1.5,'A'),(35,19,1,3,'EC','G401','TH',1430,1600,1.5,'A'),(36,17,1,6,'S14','G101','MW',1245,1415,1.5,'A'),(37,18,1,1,'EK','G101','MW',1245,1415,1.5,'A'),(38,3,1,7,'S17','G302B','TH',0915,1045,1.5,'A'),(39,1,1,2,'S18','G301A','TH',0730,0900,1.5,'A'),(40,19,1,3,'EC','G401','TH',1430,1600,1.5,'A'),(41,17,1,6,'S14','G101','MW',1245,1415,1.5,'A'),(42,18,1,1,'EK','G101','MW',1245,1415,1.5,'A'),(43,3,1,7,'S17','G302B','TH',0915,1045,1.5,'A'),(44,1,1,2,'S18','G301A','TH',0730,0900,1.5,'A'),(45,19,1,3,'EC','G401','TH',1430,1600,1.5,'A');
 /*!40000 ALTER TABLE `courseoffering` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -232,7 +231,7 @@ CREATE TABLE `faculty` (
   `status` char(1) NOT NULL,
   PRIMARY KEY (`facref_no`),
   UNIQUE KEY `facref_no_UNIQUE` (`facref_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,7 +240,7 @@ CREATE TABLE `faculty` (
 
 LOCK TABLES `faculty` WRITE;
 /*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
-INSERT INTO `faculty` VALUES (1,'97055286','Kevin','A.','Dumalay','kevin_dumalay@dlsu.edu.ph',1001,'A'),(2,'11292828','Star','R.','Yu','star_yu@dlsu.edu.ph',1002,'A'),(3,'97066783','Kyle','B. ','Chua','kyle_chua@dlsu.edu.ph',1004,'A'),(4,'20145489','Ted','C.','Lim','ted_lim@dlsu.edu.ph',1003,'A'),(5,'97026012','Ralph','S.','Bravante','ralphchristianbravante@yahoo.com',1006,'A');
+INSERT INTO `faculty` VALUES (1,'97055286','Kevin','A.','Dumalay','kevin_dumalay@dlsu.edu.ph',1001,'A'),(2,'11292828','Star','R.','Yu','star_yu@dlsu.edu.ph',1002,'A'),(3,'97066783','Kyle','B. ','Chua','gedrite.agustin@live.com',1004,'A'),(4,'20145489','Ted','C.','Lim','ted_lim@dlsu.edu.ph',1003,'A'),(5,'97026012','Ralph','S.','Bravante','ralphchristianbravante@yahoo.com',1006,'A'),(6,'11428066','Jason','A.','Sy','jason_sy@dlsu.edu.ph',1004,'A'),(7,'98011001','Florante','B.','Salvador','florante_salvador@dlsu.edu.ph',1002,'A');
 /*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -277,6 +276,30 @@ LOCK TABLES `makeup` WRITE;
 /*!40000 ALTER TABLE `makeup` DISABLE KEYS */;
 INSERT INTO `makeup` VALUES (6,2,900,1030,1.5,'g203','AC','2016-12-05','2016-12-05','Ged','A'),(8,1,1100,1400,3,'A401','FT','2016-12-05','2016-12-05','Ged','A'),(11,2,1430,1600,1.5,'g102','FT','2016-12-07','2016-12-07','Jason','A'),(13,2,1245,1415,1.5,'G210','XX','2016-12-08','2016-12-07','Jason','A'),(14,2,1200,1330,1.5,'g401','OB','2016-12-07','2016-12-07','Jason','D'),(16,2,1230,1430,2,'G401','XX','2016-12-16','2016-12-16','Kyle Chuapapi','A'),(17,2,1230,1430,2,'G401','XX','2016-12-16','2016-12-16','Kyle Chuapapi','A');
 /*!40000 ALTER TABLE `makeup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reason`
+--
+
+DROP TABLE IF EXISTS `reason`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reason` (
+  `reason_cd` char(2) NOT NULL,
+  `reason_desc` varchar(45) NOT NULL,
+  PRIMARY KEY (`reason_cd`),
+  UNIQUE KEY `reason_cd_UNIQUE` (`reason_cd`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reason`
+--
+
+LOCK TABLES `reason` WRITE;
+/*!40000 ALTER TABLE `reason` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reason` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -345,7 +368,7 @@ CREATE TABLE `term` (
   `status` char(1) NOT NULL,
   PRIMARY KEY (`termid`),
   UNIQUE KEY `termid_UNIQUE` (`termid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -354,7 +377,7 @@ CREATE TABLE `term` (
 
 LOCK TABLES `term` WRITE;
 /*!40000 ALTER TABLE `term` DISABLE KEYS */;
-INSERT INTO `term` VALUES (1,2,1,'2016-08-28','2016-12-20','A');
+INSERT INTO `term` VALUES (1,2,1,'2016-08-28','2016-12-20','A'),(2,1,1,'2015-05-23','2015-08-25','R'),(3,1,2,'2015-09-14','2015-12-14','R'),(4,1,3,'2016-01-05','2016-04-02','R');
 /*!40000 ALTER TABLE `term` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -367,4 +390,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-16  8:42:49
+-- Dump completed on 2016-12-17 21:55:54
